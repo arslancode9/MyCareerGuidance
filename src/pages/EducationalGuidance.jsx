034d57547f8377b2f39edea1cf45965e000143bc
arslancode9 <muhammadarslanpk67@gmail.com>
2025-11-17 00:05:26 +0500

    ui upadte

--- src/pages/EducationalGuidance.jsx
+++ src/pages/EducationalGuidance.jsx
@@ -39,7 +39,7 @@ export default function EducationalGuidance() {
   };
 
   return (
-    <div className="min-h-screen mt-24 bg-gray-50 p-4 sm:p-6 lg:p-8">
+    <div className="min-h-screen mt-23 bg-gray-50 p-4 sm:p-6 lg:p-8">
       <div className="max-w-6xl mx-auto">
         {/* Header */}
         <div className="text-center mb-8">
